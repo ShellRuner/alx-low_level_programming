6d1c2601831d02bc5f193eeb48bbf6b9e2a34048
WisboyAk47 <sitouamu510@gmail.com>
2023-07-21 19:48:33 -0700

    mul function done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - The function 
+ *
+ * @a: Firt argument
+ *
+ * @b: Second argument
+ *
+ * Return: Always res
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,5 @@ Task0 :0.isupper
     Write a function that checks for uppercase character
 
 Task1 :1.isdigit
+
+Task2 :
