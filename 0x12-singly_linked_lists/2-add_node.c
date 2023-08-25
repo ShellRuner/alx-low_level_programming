7be0b34414f84b11cceb658adde194e208a561e3
WisboyAk47 <sitouamu510@gmail.com>
2023-08-24 19:56:37 -0700

    In process

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
@@ -26,8 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
-	for (len = 0; str[len];)
-		len++
+	for (len = 0; str[len]; len++)
 
 		new->str = vup;
 		new->len = len;
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *free_list - Frees a list_t list
+ *
+ *@head: A pointer to the list_t list
+ *
+ * Return:Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+
+		head = tmp;
+	}
+}
