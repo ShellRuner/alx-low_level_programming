9ceeb9b9f3de300190590ec02ad29b3d55f52112
ShellRuner <sitouamu510@gmail.com>
2023-09-02 16:09:26 -0700

    print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - A function that prints a string in reverse
+ *
+ * @s: Parameter of the function
+ *
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*(s - 1));
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,4 +2,7 @@
    Write a function that prints a string .followed by a new line.
    Prototypes: void _puts_recursion(char *s);
 
+***Task1: Why is it so important to dream? Because, in my dreams we are together
+   Write a function that prints a string in reverse. Prototype : void _print_rev_recursion(char *s);
+
 
