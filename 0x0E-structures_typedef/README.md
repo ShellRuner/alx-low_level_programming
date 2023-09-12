7d047f8f88cb4381fff8e72f38fbd527e99779af
ShellRuner <sitouamu510@gmail.com>
2023-09-11 19:24:40 -0700

    creat poppy type

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+***Task0:Poppy
+   Define a new type struct dog with the following elments:
+   name , type = char *
+   age , type = float
+   owner , type = char *
+
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - represent a dog identifier
+ *
+ * @name: parameter that handle the name of the dog
+ *
+ * @age : age of the dog
+ *
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
