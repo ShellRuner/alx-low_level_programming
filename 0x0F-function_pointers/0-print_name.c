bf74e04645a1ae36ff521d8e7086fadc37e926aa
ShellRuner <sitouamu510@gmail.com>
2023-09-15 05:51:33 -0700

    print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name
+ *
+ * @name: A pointer that point on the name of the person
+ *
+ * @f: a function pointer
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
