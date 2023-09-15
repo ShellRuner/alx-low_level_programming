8afbebd75a3ab002b357457b4e3aeebf0d98ef52
ShellRuner <sitouamu510@gmail.com>
2023-09-15 06:04:35 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -11,5 +12,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name != NULL)
+		(*f)(name);
 }
