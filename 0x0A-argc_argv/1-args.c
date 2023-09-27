aefd0c2dcf3f25522076946023364962260aa244
ShellRuner <sitouamu510@gmail.com>
2023-09-27 20:01:00 +0000

    print number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @args: a param
+ * @argv: a param
+ *
+ * Return: value
+ */
+int main(int args, char *argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+	return (0);
+}
