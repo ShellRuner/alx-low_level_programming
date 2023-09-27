ee712f539b7355342d9d794c09ce5977a7e85bd4
ShellRuner <sitouamu510@gmail.com>
2023-09-27 19:56:28 +0000

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A function
+ * @argc: a param
+ * @argv: a param
+ *
+ * Return: value
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
