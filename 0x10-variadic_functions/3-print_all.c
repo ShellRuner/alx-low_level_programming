ed40124baa208961521b696c2f1a9290d490f259
ShellRuner <sitouamu510@gmail.com>
2023-09-27 15:21:17 +0000

    print anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - A function that print anything
+ * @format: Fixe parameter
+ *
+ * Return: A value
+ */
+void print_all(const char * const format, ...)
+{
+	va_list v1;
+	char *string;
+	int i;
+
+	i = 0;
+	va_start(v1, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%i", va_arg(v1, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(v1, double));
+				break;
+			case 'c':
+				printf("%c", (char) va_arg(v1, int));
+				break;
+			case 's':
+				string = va_arg(v1, char *);
+				if (string == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", string);
+				break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') && format[(i + 1)] != '\0')
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(v1);
+}
