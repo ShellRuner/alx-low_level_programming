1f72b377dcc27bd6395e0852c51f20f2def71310
ShellRuner <sitouamu510@gmail.com>
2023-09-27 16:34:35 +0000

    initialize structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct dog - represent a dog identifier
  *
@@ -16,6 +18,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 
 #endif
