f66687c40959c10e00bf33cebe5d6a7e3a87e3b4
ShellRuner <sitouamu510@gmail.com>
2023-09-27 20:52:58 +0000

    Task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - A function
+ * @width: param
+ * @height: param
+ *
+ * Return: value
+ */
+int **alloc_grid(int width, int height)
+{
+	int j, i;
+	int **arr;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	arr = (int **) malloc(sizeof(int *) * height);
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
