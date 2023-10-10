3cbec799bba578d26b6f9068714c1f8592c798b3
ShellRuner <sitouamu510@gmail.com>
2023-10-10 18:03:31 +0000

    Task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,8 +24,9 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		if (text_content != NULL || == NULL)
-			bytes_write = write(fd, buffer, strlen(text_content));
+		if (text_content == NULL)
+			bytes_write = write(fd, buffer, 0);
+		bytes_write = write(fd, buffer, strlen(text_content));
 		if (bytes_write == -1)
 		{
 			return (-1);
