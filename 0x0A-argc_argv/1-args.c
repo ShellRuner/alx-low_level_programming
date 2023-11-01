d5dd4a72307d30aa0447dc7659b89fbff0396826
ShellRuner <sitouamu510@gmail.com>
2023-11-01 22:36:49 +0000

    number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: the number of argument
+ * @argv: the parameter the handle the arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argv[0])
+		printf("%d\n", argc);
+	return (0);
+}
