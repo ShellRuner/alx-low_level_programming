57cff3d09a8b8e0fd1bd705da307c14ab30ccc79
ShellRuner <sitouamu510@gmail.com>
2023-11-07 19:23:50 +0000

    print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,21 +8,22 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
-		if (d->name == NULL)
-		{
-			printf("Name: (nil)\n");
-		}
-		else if (d->owner == NULL)
-		{
-			printf("Owner: (nil)\n");
-		}
-		else
-		{
-			printf("Name: %s\n", d->name);
-			printf("Age: %f\n", d->age);
-			printf("Owner: %s\n", d->owner);
-		}
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 }
