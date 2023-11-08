9e72aa5f43c251006c59537a6df79e2baaa63ba6
ShellRuner <sitouamu510@gmail.com>
2023-11-08 11:46:48 +0000

    Tsk3&4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+
+/**
+ * new_dog - A function that creates a new dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: A pointer to a new dog ( success) or NUll
+ *         (fall)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL || ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptr->name = name;
+		ptr->age = age;
+		ptr->owner = owner;
+		return (ptr);
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -10,4 +10,11 @@ name ,type= char * ; age,type= float; owner,type= char *
    Write a function that prints a struct dog
    prototype: void print_dog(struct dog *d);
 
+***Task3: Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read
+   Define a new type dog_t as a new name for the type struct dog
 
+***Task4: A door is what a dog is perpetually on the wrong side of
+  Write a function that creates a new dog
+  protype: dog_t *new_dog(char *name, float age, char *owner);
+
+***Task5: 
