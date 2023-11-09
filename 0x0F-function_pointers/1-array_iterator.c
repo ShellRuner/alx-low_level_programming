4fa5e9bf706efebfc860ebdab511018cc40263a0
ShellRuner <sitouamu510@gmail.com>
2023-11-09 12:47:02 +0000

    Task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - A function that executes a function given
+ *                  as a parameter on each elmt f array
+ * @array: An array of integer
+ * @size: the size of the array
+ * @action: a function pointer that execute fct on elmt of the
+ *          array
+ *
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (array[i] && i < size)
+	{
+		(*action)(array[i]);
+		i++;
+	}
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -2,4 +2,8 @@
    Write a function that prints a name
    prototype: void print_name(char *name, void (*f)(char *));
 
-**Task1:
+**Task1: If you spend too much time thinking about a thing, you'll never get it done
+   Write a function that executes a function given as parameter on each elmt of an array
+   prototype: void array_iterator(int *array, size_t size, void (*action)(int));
+
+**Task2: 
