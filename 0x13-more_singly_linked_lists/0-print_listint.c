86d1418e2e7cf6c27f0b64c6cb2793b46dc76a5f
ShellRuner <sitouamu510@gmail.com>
2023-11-22 15:20:06 +0000

    Task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - A function that prints all the element
+ *                 of a listint_t list
+ * @h: a pointer to the listint_t list to print
+ *
+ * Return: the number of node
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		if (h->n)
+		{
+			printf("%d\n", h->n);
+		}
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+***Task0: Print list
+   Write a function that prints all the elements of a listint_t list
+  Prototype: size_t print_lstint(const listint_t *h);
