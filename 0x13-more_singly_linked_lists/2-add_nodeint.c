eb8c11b22ccda06186000447fde2abe15e997324
ShellRuner <sitouamu510@gmail.com>
2023-11-22 16:37:58 +0000

    task2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - A function that adds anew node at the
+ *               beginning of a listint_t list
+ * @head: a pointer to the list
+ * @n: an int
+ *
+ * Return:the address of the new element or NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -6,4 +6,6 @@
     Write a function that returns the number of elements in a listint_t list
    Prototype: size_t listint_len(const listint_t *h);
 
-
+***Task2: Add node
+  Write a function that adds a new node at the beginning of a listint_t list;
+  prototype: listint_t *add_nodeint(listint_t **head, const int n);
