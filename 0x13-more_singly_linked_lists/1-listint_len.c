379f325f9eded11b9f938ab829f00353c29f0758
ShellRuner <sitouamu510@gmail.com>
2023-11-22 15:33:49 +0000

    Task1 solved

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,7 +5,7 @@
  *               elements in a linked listint_t
  *@h: A pointer to the listint_t list
  *
- * return: the number of elements in the list
+ * Return: the number of elements in the list
  */
 size_t listint_len(const listint_t *h)
 {
