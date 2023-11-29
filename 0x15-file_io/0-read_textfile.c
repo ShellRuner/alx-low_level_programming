2cf1a8c3dcf52585a21dd4f794f11a595f34fe73
ShellRuner <sitouamu510@gmail.com>
2023-11-29 13:10:35 +0000

    Task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function that reads a text file and
+ *                 prints it to the POSIX stdout
+ * @filename: the file name
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the number of letters it could read and print
+ *         or 0(fail)
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int count;
+	int fd, br;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+	fd = open(filename, O_RDONLY, 0640);
+	if (buf == NULL)
+		return (0);
+	if (fd == -1)
+		return (0);
+	br = read(fd, buf, letters);
+	if (br == -1)
+		return (0);
+	count = write(1, buf, br);
+	close(fd);
+	free(buf);
+	if (br != count || count == -1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (count);
+	}
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+***Task0: Tread lightly, she is near
+     Write a function that read a_textfile(const char *filename, size_t letters);
+
+
