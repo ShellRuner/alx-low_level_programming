d369cef7e2dd038dd18d47e638c0df1fade7e7a1
ShellRuner <sitouamu510@gmail.com>
2024-03-27 00:25:08 +0000

    Task5 submit

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,10 +10,15 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i = 0, idmax;
-	dlistint_t *cur;
+	unsigned int i = 0, idmax = 0;
+	dlistint_t *cur, *ptr;
 
-	idmax = dlist_len(head);
+	ptr = head;
+	while (ptr != NULL)
+	{
+		idmax++;
+		ptr = ptr->next;
+	}
 	if (index == 0)
 	{
 		if (head == NULL)
